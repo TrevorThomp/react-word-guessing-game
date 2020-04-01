c3ff9f2fb15c23f9a7b0496ad9a42b79fd36ce33
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2020-04-01 12:40:49 -0700

    added splash component

---
+++ src/components/splash/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Splash(){
+  return (
+    <div id="overlay" class="start">
+    <h2 class="title">Wheel of Phrases</h2>
+    <a class="btn__reset">Start Game</a>
+  </div>
+  )
+}
+
+export default Splash;
